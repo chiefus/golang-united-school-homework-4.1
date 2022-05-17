64ee7eefe30e420714c58d322c8ceb57c8b2d47e
Andrei Shalkou <andrei.shalkou@camelotls.com>
2022-05-17 16:57:02 +0300

    Fixed issue with cyrilic string length

--- reverse_string.go
+++ reverse_string.go
@@ -2,7 +2,7 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	inputSlice := []rune(input)
-	strLen := len(input) - 1
+	strLen := len(inputSlice) - 1
 	var outputSlice []rune
 	for i := strLen; i > -1; i-- {
 		outputSlice = append(outputSlice, inputSlice[i])
