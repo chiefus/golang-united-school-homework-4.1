4d7493af8ef63e6993d94530a17ed6978eb99b28
Andrei Shalkou <andrei.shalkou@camelotls.com>
2022-05-17 16:51:50 +0300

    ReverseString function implemented

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,12 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	inputSlice := []rune(input)
+	strLen := len(input) - 1
+	var outputSlice []rune
+	for i := strLen; i > -1; i-- {
+		outputSlice = append(outputSlice, inputSlice[i])
+	}
+	output = string(outputSlice)
 	return output
 }
